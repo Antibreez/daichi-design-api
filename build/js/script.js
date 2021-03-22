6320f2d37c453ef85613fef7f6357670300f15bb
Anton Kiyan <kiyananton@gmail.com>
2021-03-23 00:57:48 +0300

    initial commit

---
+++ build/js/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+(function () {
+  var inputs = document.querySelectorAll('.input-phone');
+
+  if (!inputs[0]) {
+    return;
+  }
+
+  var im = new Inputmask("+7(999) 999-99-99");
+  inputs.forEach(function (item) {
+    im.mask(item);
+  });
+})();
